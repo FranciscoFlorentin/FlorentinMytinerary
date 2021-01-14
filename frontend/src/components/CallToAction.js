c18d474f339106ea83ea789c5e75609b1c712fee
Fran <franciscoaflorentin@hotmail.com>
2021-01-14 18:21:31 -0300

    Agregue fotos de la Section

--- frontend/src/components/CallToAction.js
+++ frontend/src/components/CallToAction.js
@@ -3,8 +3,13 @@ import {Link} from "react-router-dom";
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 const CallToAction = () => {
     return (
-        <div className="divCall">
-            <Link><button className="botonCall"><h6>Discover more Itineraries</h6><ArrowForwardIcon color="primary" /></button></Link>
+        <div className="divCall1">
+            <div className="divCall2">
+                <Link><button className="botonCall"><h6>Discover more Itineraries</h6><ArrowForwardIcon color="primary" /></button></Link>
+            </div>
+            <div style={{width:"50%",backgroundImage:"url('./assets/Screenshot_2.png')",
+                backgroundSize:"contain", backgroundRepeat:"no-repeat",backgroundPositionX:"80%"}}>
+            </div>
         </div>
     )
 }
--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -11,7 +11,9 @@ const Logo=()=>{
                 height: "30vh"}}>
             </div>
             <div style={{display:"flex"}}>
-                <div style={{width:"50%"}}></div>
+                <div style={{width:"50%",backgroundImage:"url('./assets/Screenshot_1.png')",
+                    backgroundSize:"contain", backgroundRepeat:"no-repeat",backgroundPositionX:"50%"}}>
+                </div>
                 <h4 class="textoLogo" style={{alignContent:"right", width:"50%", textAlign:"right", padding:""}}>Find your perfect trip,<br></br> designed by insiders <br></br>who know and love<br></br> their cities</h4>
             </div>
         </>
