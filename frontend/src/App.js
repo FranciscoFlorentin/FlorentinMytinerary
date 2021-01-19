2775238a752f89ebf3ea343b8146f457576d960b
Fran <franciscoaflorentin@hotmail.com>
2021-01-19 16:15:06 -0300

    Cree componente Itinerarie y añadi Links a las ciudades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,20 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
 import Cities from "./components/Cities";
-import {BrowserRouter,Route} from "react-router-dom";
+import Itinerarie from "./components/Itinerarie"
+import {BrowserRouter,Redirect,Route,Switch} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Route exact path="/" component={HomePage} />
-        <Route path="/cities" component={Cities} />
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/cities" component={Cities} />
+          <Route path="/itineraries/:id" component={Itinerarie} />
+          <Redirect to="/" />
+        </Switch>
         <Footer />
       </BrowserRouter>
     </div>
--- frontend/src/components/City.js
+++ frontend/src/components/City.js
@@ -5,7 +5,8 @@ export default class City extends Component {
         return (
             <>
                 <div className="city1">
-                    <Link to="/">
+                    {/* cambio el url por /itineraries/(nombre de la ciudad sin espacio) */}
+                    <Link to={`/itineraries/${(this.props.city.name).replace(/ /g, "")}`}>
                         <div style={{backgroundImage:`url("${this.props.city.dir}")`}}> 
                             {<h5>{this.props.city.name}</h5>}
                         </div>
