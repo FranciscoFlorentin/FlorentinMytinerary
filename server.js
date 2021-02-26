12ed5aff4fd64f32f1ee9f39362b173b46f0d8b0
Fran <franciscoaflorentin@hotmail.com>
2021-02-26 10:35:19 -0300

    asd

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api",router);
 
-const port=process.env.PORT;
+const port=process.env.PORT || 4000;
 const host=process.env.HOST || "0.0.0.0";
-app.listen(port,()=>console.log("App listening on port 4000"));
+app.listen(port,host,()=>console.log("App listening on port 4000"));
